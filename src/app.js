0b955fdc67b1b77d2f6a7afecf3c3379bc953556
Wasif Mehmood <wasifmehmood20@gmail.com>
2023-09-24 14:58:30 +0500

    Project structure changed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const http = require('http');
-const { port } = require('./config/app_config.js');
 
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const { port } = require('./config/app_config');
 
 const app = express();
 app.use(cors());
@@ -15,7 +14,7 @@ require('./utils/database.js');
 require('./models/index.js');
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`);
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.use(routes);
@@ -23,5 +22,5 @@ app.use(routes);
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
--- src/config/app_config.js
+++ src/config/app_config.js
@@ -6,7 +6,7 @@ module.exports = {
     port: process.env.PORT || 3000,
     DB_HOST: process.env.DB_HOST || '127.0.0.1',
     DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || '',
     SERVICE_TWILIO_ACCOUNT_ID: process.env.SERVICE_TWILIO_ACCOUNT_ID || '',
     SERVICE_TWILIO_AUTH: process.env.SERVICE_TWILIO_AUTH || '',
     SERVICE_TWILIO_SERVICE_ID: process.env.SERVICE_TWILIO_SERVICE_ID || '',
